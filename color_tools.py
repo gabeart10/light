eccf3ef613c44ea02f141d34f26e9a6a9702d51c
gabeart10 <millergabe10@gmail.com>
2015-07-09 12:04:18 -0400

    save

--- color_tools.py
+++ color_tools.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+#Thes are my GPIO tools!
+
 
 def all_off(p):
     for pin in p:
@@ -8,3 +10,55 @@ def all_out(op):
     GPIO.setmode(GPIO.BCM)
     for pin in op:
         GPIO.setup(pin, GPIO.OUT)
+
+def red_on(r,g,b):
+    GPIO.output(r, 1)
+
+def green_on(r,g,b):
+    GPIO.output(g, 1)
+
+def blue_on(r,g,b):
+    GPIO.output(b, 1)
+
+def cyan_on(r,g,b):
+    GPIO.output(b, 1)
+    GPIO.output(g, 1)
+
+def magenta_on(r,g,b):
+    GPIO.output(r, 1)
+    GPIO.output(b, 1)
+
+def yellow_on(r,g,b):
+    GPIO.output(r, 1)
+    GPIO.output(g, 1)
+
+def white_on(r,g,b):
+    GPIO.output(r, 1)
+    GPIO.output(g, 1)
+    GPIO.output(b, 1)
+ 
+def white_off(r,g,b):
+    GPIO.output(r, 0)
+    GPIO.output(g, 0)
+    GPIO.output(b, 0)
+
+def red_off(r,g,b):
+    GPIO.output(r, 0)
+
+def green_off(r,g,b):
+    GPIO.output(g, 0)
+
+def blue_off(r,g,b):
+    GPIO.output(b, 0)
+
+def cyan_off(r,g,b):
+    GPIO.output(g, 0)
+    GPIO.output(b, 0)
+
+def magenta_off(r,g,b):
+    GPIO.output(r, 0)
+    GPIO.output(b, 0)
+
+def yellow_off(r,g,b):
+    GPIO.output(r, 0)
+    GPIO.output(g, 0)
